ceab5f5d776a91a04757416778618e735c283e55
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2021-11-02 11:07:04 +0900

    add time slider

--- main_an.py
+++ main_an.py
@@ -17,7 +17,8 @@ class videoFile:
 
 
 def select_video():
-    global panel
+    global panel, isPlay, timer, timeBox
+
     root.file = filedialog.askopenfile(
         initialdir='path',
         title='select video file',
@@ -27,6 +28,7 @@ def select_video():
     )
     if '.avi' in root.file.name or '.mp4' in root.file.name:
         targetV.name = root.file.name
+        isPlay = True
 
         t_cap = cv.VideoCapture(targetV.name)
         t_ret, t_frame = t_cap.read()
@@ -38,19 +40,37 @@ def select_video():
         else:
             panel.configure(image=thumbnail)
             panel.image = thumbnail
+
+        fps = t_cap.get(cv.CAP_PROP_FPS)
+        num_frame = t_cap.get(cv.CAP_PROP_FRAME_COUNT)
+        runtime = num_frame / fps
+        if timer is None:
+            timer = Scale(timer_frame, orient='horizontal', showvalue=True, from_=0, to_=runtime)
+            timer.grid(row=0, column=0)
+        else:
+            timer.configure(orient='horizontal', showvalue=True, from_=0, to_=runtime)
+        if timeBox is None:
+            timeBox = Label(timer_frame, text='00:00')
+            timeBox.grid(row=0, column=1)
+        else:
+            timeBox.configure(text='00:00')
+
     else:
         messagebox.showinfo(title="Selection Fail", message="This is invalid file format. Please select avi or mp4"
                                                             "file.")
+        targetV.name = ""
 
 
 def tracking_video(vfile):
-    global panel
+    global panel, isPlay, timer
 
     if vfile == "":
         messagebox.showinfo(title="Video Load Error", message="Please select video before tracking.")
         return
 
     cap = cv.VideoCapture(vfile)
+    start_time = timer.get() * 1000
+    cap.set(cv.CAP_PROP_POS_MSEC, start_time)
     # Parameters for lucas kanade optical flow
     lk_params = dict(winSize=(30, 30),
                      maxLevel=2,
@@ -73,6 +93,7 @@ def tracking_video(vfile):
     mask = np.zeros_like(old_frame)
     vector = np.array([0, 0])
     cv.destroyWindow('Select Window')
+    isPlay = True
     while cap.isOpened():
         ret, frame = cap.read()
         frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -110,7 +131,7 @@ def tracking_video(vfile):
         # Now update the previous frame and previous points
         old_gray = frame_gray.copy()
         points = good_new.reshape(-1, 1, 2)
-        if cv.waitKey(30) == 27:
+        if cv.waitKey(30) == 27 or isPlay is False:
             return
 
 
@@ -120,14 +141,51 @@ def videoThreadStart():
     thread_video.start()
 
 
-targetV = videoFile("")
+def setVideoTime():
+    global panel, timer, timeBox
 
+    if targetV.name == "":
+        messagebox.showinfo(title="Video Load Error", message="Please select video before setting video time.")
+        return
+    else:
+        input_time = timer.get()
+        minutes = int(input_time / 60)
+        seconds = int(input_time % 60)
+        timeBox.configure(text=str.format("{0}:{1}", minutes, seconds))
+
+        temp_cap = cv.VideoCapture(targetV.name)
+        temp_cap.set(cv.CAP_PROP_POS_MSEC, input_time * 1000)
+        ret_temp, frame_temp = temp_cap.read()
+        newthumb = ImageTk.PhotoImage(Image.fromarray(cv.cvtColor(frame_temp, cv.COLOR_BGR2RGB)))
+        if panel is None:
+            panel = Label(image=newthumb)
+            panel.image = newthumb
+            panel.pack()
+        else:
+            panel.configure(image=newthumb)
+            panel.image = newthumb
+
+
+def videoStop():
+    global isPlay
+
+    if targetV.name != "" and isPlay is True:
+        isPlay = False
+
+
+targetV = videoFile("")
 panel = None
+timer = None
+timeBox = None
+isPlay = True
 
 root = Tk()
 btn_frame = Frame(root)
+timer_frame = Frame(root)
 btn1 = Button(btn_frame, text="Select Video", command=select_video)
 btn2 = Button(btn_frame, text="Tracking", command=videoThreadStart)
+btn3 = Button(btn_frame, text="Setup Time", command=setVideoTime)
+btn4 = Button(btn_frame, text="Stop Video", command=videoStop)
 
 
 if __name__ == "__main__":
@@ -136,7 +194,10 @@ if __name__ == "__main__":
     root.resizable(False, False)
 
     btn_frame.pack(side=tkinter.BOTTOM)
+    timer_frame.pack(side=tkinter.BOTTOM)
     btn1.grid(row=0, column=0)
     btn2.grid(row=0, column=1)
+    btn3.grid(row=0, column=2)
+    btn4.grid(row=0, column=3)
 
     root.mainloop()
